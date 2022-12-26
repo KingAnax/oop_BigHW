e37e1ca54a6b58afae9fcbf20a8d3b59572ed109
KingAnax <102864013+KingAnax@users.noreply.github.com>
2022-12-26 17:13:45 +0800

    Add files via upload

---
+++ include/common_file_opr.h
@@ -0,0 +1,12 @@
+/*2151613 Ã«ÓîÂ¼ ¼Æ¿Æ*/
+#pragma once
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include "../include/cmd_console_tools.h"
+using namespace std;
+
+int file_size(ifstream& fin);
+int tp(int file_size);
---
+++ include/common_my_gmw_tools.h
@@ -0,0 +1,9 @@
+/*2151613 Ã«ÓîÂ¼ ¼Æ¿Æ*/
+#pragma once
+#define KB_PAGE_UP   73
+#define KB_PAGE_DOWN 81
+
+#include "cmd_gmw_tools.h"
+#include <Windows.h>
+
+void my_read_keyboard(int& keycode1, int& keycode2);
